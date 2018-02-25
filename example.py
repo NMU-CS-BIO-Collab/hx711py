d240091a3a74a6ccbfa70dc78bdc245d598065b7
Peter Maurer <33104257+p-maurer@users.noreply.github.com>
2018-02-24 21:07:12 -0500

    Working to get proper units

--- example.py
+++ example.py
@@ -3,11 +3,13 @@ import time
 import sys
 from hx711 import HX711
 
+
 def cleanAndExit():
     print "Cleaning..."
     GPIO.cleanup()
     print "Bye!"
     sys.exit()
+
 
 hx = HX711(5, 6)
 
@@ -24,9 +26,12 @@ hx.set_reading_format("LSB", "MSB")
 # To set the reference unit to 1. Put 1kg on your sensor or anything you have and know exactly how much it weights.
 # In this case, 92 is 1 gram because, with 1 as a reference unit I got numbers near 0 without any weight
 # and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
-# If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
-#hx.set_reference_unit(113)
-hx.set_reference_unit(92)
+# If 2000 grams is 184000 then 1 grams is 184000 / 2000 = 92.
+# hx.set_reference_unit(113)
+
+# was 92 -Peter
+# 532
+hx.set_reference_unit(592)
 
 hx.reset()
 hx.tare()
@@ -36,10 +41,10 @@ while True:
         # These three lines are usefull to debug wether to use MSB or LSB in the reading formats
         # for the first parameter of "hx.set_reading_format("LSB", "MSB")".
         # Comment the two lines "val = hx.get_weight(5)" and "print val" and uncomment the three lines to see what it prints.
-        #np_arr8_string = hx.get_np_arr8_string()
-        #binary_string = hx.get_binary_string()
-        #print binary_string + " " + np_arr8_string
-        
+        # np_arr8_string = hx.get_np_arr8_string()
+        # binary_string = hx.get_binary_string()
+        # print binary_string + " " + np_arr8_string
+
         # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
         val = hx.get_weight(5)
         print val
